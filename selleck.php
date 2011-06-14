6f6cbe376774ffd092979910b973d869bfd23b09
Gary Rennie <webmaster@gazler.com>
2011-06-14 12:40:41 +0100

    initial

---
+++ selleck.php
@@ -0,0 +1,36 @@
+<?php
+require 'mustache.php';
+class Selleck extends Mustache
+{
+    private $_directory = "templates/";
+    private $_loaded = array();
+
+    public function render($template, $data)
+    {
+        return parent::render($this->load($template), $data);
+    }
+
+    public function js_template($template, $id)
+    {
+        return '<script type="text/html" id="'.$id.'">'."\n".$this->load($template).'</script>'."\n";
+    }
+
+    private function load($template)
+    {
+        if (!array_key_exists($template, $this->_loaded))
+        {
+            $this->_loaded[$template] = file_get_contents($this->_directory.$template.".mustache");
+        }
+        return $this->_loaded[$template];
+    }
+}
+
+
+
+$t = new Selleck();
+
+echo $t->render("index", array('name' => 'test'));
+
+echo $t->js_template("index", "index_template");
+?>
+
